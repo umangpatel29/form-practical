bd56176e0e09202598fe4f1a56953541eb97c406
umangpatel29 <128145483+umangpatel29@users.noreply.github.com>
2023-10-05 13:38:22 +0530

    form-practical

---
+++ formzip/src/Form/Table.js
@@ -0,0 +1,47 @@
+import React, { useEffect, useState } from 'react'
+import Table from 'react-bootstrap/Table';
+
+
+const TableData = ({ data, onDelete, onEdit }) => {
+    console.log(data)
+    return (
+        <div>
+            <Table>
+                <thead>
+                    <tr>
+                        <th>name</th>
+                        <th>email</th>
+                        <th>price</th>
+                        <th>quantity</th>
+                        <th>city</th>
+                        <th>state</th>
+                        <th>zipcode</th>
+                        <th>action</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {
+                        data && data.map((item, id) => (
+                            <tr key={id}>
+                                <td>{item.name}</td>
+                                <td>{item.email}</td>
+                                <td>{item.price}</td>
+                                <td>{item.quantity}</td>
+                                <td>{item.city}</td>
+                                <td>{item.state}</td>
+                                <td>{item.zipcode}</td>
+                                <td>
+                                    <button onClick={()=> onEdit(id, item)}>edit</button>
+                                    <button onClick={()=> onDelete(id)}>delete</button>
+                                </td>
+                            </tr>
+                        ))
+                    }
+
+                </tbody>
+            </Table>
+        </div >
+    )
+}
+
+export default TableData
